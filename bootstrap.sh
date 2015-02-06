0c4ac478b3acf3a8e020dc070cfb1d72677eb36d
David Hull <david.hull@mbp-109-op-04.corp.openx.com>
2015-02-06 15:57:51 -0800

    bootstrap.sh: Set hostname based on box name.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,21 +2,37 @@
 
 # Vagrant runs this script inside the newly-created VM to customize it.
 
+HOSTNAME=$(perl -e '$hn = lc(shift); $hn =~ s/[^a-z0-9]+/-/g; print $hn;' "$1")
+
 . /vagrant-setup/config
 
-set -x
-
 echo "Provisioning from bootstrap.sh"
-if ! egrep "^$USERNAME:" /etc/passwd; then
+
+# Set hostname.
+if egrep '^HOSTNAME=' /etc/sysconfig/network; then
+    perl -i -p \
+         -e 'BEGIN { $hn = shift; }' \
+         -e 's/^HOSTNAME=\K.*/$hn/;' \
+         "$HOSTNAME" /etc/sysconfig/network
+fi
+if test -f /etc/hostname; then
+    echo "$HOSTNAME" >/etc/hostname
+fi
+hostname "$HOSTNAME"
+
+# Create the user if he doesn't already exist.
+if ! egrep "^$USERNAME:" /etc/passwd >/dev/null; then
+    echo "Creating user $USERNAME"
     useradd "${USERADD_EXTRA[@]}" $USERNAME
     (su $USERNAME -c 'cd $HOME && tar xzf /vagrant-setup/userhome.tar.gz')
 fi
 
+# Link the framewerk setup script if it doesn't already exist.
 if ! test -e /home/$USERNAME/setenv-fw; then
     ln -s /vagrant-setup/setenv-fw /home/$USERNAME
 fi
 
-# Let $USER use sudo without a password
+# Set up sudo for user.
 USERNAME_DOTLESS=$(echo $USERNAME | tr -d '.~')
 echo "$USERNAME ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/user-$USERNAME_DOTLESS
 chmod 0440 /etc/sudoers.d/user-$USERNAME_DOTLESS
