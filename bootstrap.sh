1150d8126c8f7774dbfd7b6e60f35479c7882e14
David Hull <david.hull@openx.com>
2018-03-02 18:03:08 -0800

    bootstrap.sh: Fix $HOME/bin/jq symlinking.

--- bootstrap.sh
+++ bootstrap.sh
@@ -67,8 +67,9 @@ hostname "$HOSTNAME$DOMAINNAME"
 
 # Symlink jq (https://stedolan.github.io/jq/) into $HOME/bin.  This is
 # a statically linked Linux x86-64 executable.
-if ! test -e /home/$USERNAME/jq; then
-    ln -s /vagrant-setup/jq-linux /home/$USERNAME/jq
+mkdir -p /home/$USERNAME/bin
+if ! test -e /home/$USERNAME/bin/jq; then
+    ln -s /vagrant-setup/jq-linux /home/$USERNAME/bin/jq
 fi
 
 # Any files in /vagrant-setup/dotfiles (without the leading '.') will
